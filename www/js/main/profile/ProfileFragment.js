7529f1b1b8bc01871c3376bd775232c07d275338
Seweryn Sleczka <sewerun777@gmail.com>
2018-05-08 21:38:11 +0200

    Implemented database, fixed navigation for android

--- www/js/main/profile/ProfileFragment.js
+++ www/js/main/profile/ProfileFragment.js
@@ -1,12 +1,30 @@
 var profileFragment = {
 
     logoutButton : null,
+    addButton : null,
+    test_label : null,
+    profile_name : null,
 
     onCreate: function() { },
 
     onDeviceReady: function() {
         this.logoutButton = $("#logout_btn");
+        this.addButton = $("#add_btn");
+        this.test_label = $("#test_label");
+        this.profile_name = $("#profile_name");
+
         this.logoutButton.click(this.onClickedLogout.bind(this));
+        this.addButton.click(this.testDataBase.bind(this));
+
+        firebase.auth().onAuthStateChanged(function (user) {
+            profileFragment.profile_name.html(user.uid);
+            console.log(user.uid);
+            firebase.database().ref('test_data').once('value').then(function(snapshot) {
+                profileFragment.test_label.html(snapshot.val());
+                console.log(snapshot.val());
+            });
+        });
+
     },
 
     onClickedLogout : function () {
@@ -17,8 +35,17 @@ var profileFragment = {
         });
     },
 
+    testDataBase : function (){
+        var userId = firebase.auth().currentUser.uid;
+        firebase.database().ref('test/' + userId).set({
+            username: "xxxxx",
+            email: "sewerun777@gmail.com",
+            profile_picture : "avatar.jpg"
+        });
+    },
+
     navigateToLogin : function () {
-        window.location.href = "/view/login/login.html";
+        navigation.navigateToPath(  "/view/login/login.html" );
     }
 
 };
