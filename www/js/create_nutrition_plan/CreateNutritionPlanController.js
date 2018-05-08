c8174f15da1dd5da4110de5c5019b3122c28e85d
Jakub Talarczyk <jakub.talarczyk2@gmail.com>
2018-05-08 22:31:07 +0200

    Nutrition plan - empty

---
+++ www/js/create_nutrition_plan/CreateNutritionPlanController.js
@@ -0,0 +1,42 @@
+var controller = {
+
+    loginButton : null,
+    registerButton : null,
+    usernameInput : null,
+    passwordInput : null,
+    errorLabel : null,
+    passwordRetypeInput : null,
+
+    onCreate: function() { },
+
+    onDeviceReady: function() {
+        this.loginButton = $("#loginbtn");
+        this.registerButton = $("#registerbtn");
+        this.usernameInput = $("#login_input");
+        this.passwordInput = $("#password_input");
+        this.errorLabel = $("#errorlabel");
+        this.passwordRetypeInput = $("#password_retype_input");
+
+        this.registerButton.click(this.onRegisterClicked.bind(this));
+        this.loginButton.click(this.navigateToLogin.bind(this));
+    },
+
+    onRegisterClicked: function () {
+        var email = this.usernameInput.val();
+        var password = this.passwordInput.val();
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(function (result) {
+            controller.navigateToMain();
+        }).catch(function(error) {
+            controller.errorLabel.text(error.code + ":" + error.message);
+        });
+    },
+
+    navigateToLogin: function () {
+        window.location.href = "/view/login/login.html";
+    },
+
+    navigateToMain: function () {
+        window.location.href = "/view/main/main.html";
+    }
+
+};
