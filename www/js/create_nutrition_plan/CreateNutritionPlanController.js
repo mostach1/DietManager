f1806e4d1662c56fc59e1669d6bd6ec908221550
Jakub Talarczyk <jakub.talarczyk2@gmail.com>
2018-05-09 23:35:54 +0200

    Create nutrition plan and add product

--- www/js/create_nutrition_plan/CreateNutritionPlanController.js
+++ www/js/create_nutrition_plan/CreateNutritionPlanController.js
@@ -1,42 +1,15 @@
 var controller = {
 
-    loginButton : null,
-    registerButton : null,
-    usernameInput : null,
-    passwordInput : null,
-    errorLabel : null,
-    passwordRetypeInput : null,
-
+    addBreakfast : null,
     onCreate: function() { },
 
     onDeviceReady: function() {
-        this.loginButton = $("#loginbtn");
-        this.registerButton = $("#registerbtn");
-        this.usernameInput = $("#login_input");
-        this.passwordInput = $("#password_input");
-        this.errorLabel = $("#errorlabel");
-        this.passwordRetypeInput = $("#password_retype_input");
-
-        this.registerButton.click(this.onRegisterClicked.bind(this));
-        this.loginButton.click(this.navigateToLogin.bind(this));
+        this.addBreakfast = $("#add_breakfast");
+        this.addBreakfast.click(this.navigateToAddProduct);
     },
 
-    onRegisterClicked: function () {
-        var email = this.usernameInput.val();
-        var password = this.passwordInput.val();
-        firebase.auth().createUserWithEmailAndPassword(email, password).then(function (result) {
-            controller.navigateToMain();
-        }).catch(function(error) {
-            controller.errorLabel.text(error.code + ":" + error.message);
-        });
-    },
-
-    navigateToLogin: function () {
-        window.location.href = "/view/login/login.html";
-    },
-
-    navigateToMain: function () {
-        window.location.href = "/view/main/main.html";
+    navigateToAddProduct: function () {
+        window.location.href = "/view/add_product/add_product.html";
     }
 
 };
--- www/js/main/diet/DietFragment.js
+++ www/js/main/diet/DietFragment.js
@@ -1,9 +1,22 @@
 var dietFragment = {
 
+    chooseExistingBtn : null,
+    createYourOwnBtn : null,
     onCreate: function() { },
 
-    onDeviceReady: function() {
+     onDeviceReady: function() {
+        this.chooseExistingBtn = $("#choose_existingbtn");
+         this.createYourOwnBtn = $("#create_your_ownbtn");
+         this.chooseExistingBtn.click(this.switchTab);
+         this.createYourOwnBtn.click(this.switchToPlanCreation);
+     },
 
-    }
+     switchTab: function(){
+         var instance = M.Tabs.getInstance($('.tabs'));
+         instance.select('test-swipe-1');
+     },
 
+     switchToPlanCreation: function(){
+        window.location.href = '/view/create_nutrition_plan/create_nutrition_plan.html';
+     }
 };
