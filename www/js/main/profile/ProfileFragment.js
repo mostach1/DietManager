45e99476a23ad68b24e25443d460f31200ec464e
mostach1 <monika.stach@poczta.onet.pl>
2018-05-16 23:42:35 +0200

    profile updated and confict resolved

--- www/js/main/profile/ProfileFragment.js
+++ www/js/main/profile/ProfileFragment.js
@@ -14,7 +14,44 @@ var profileFragment = {
         this.profile_name = $("#profile_name");
 
         this.logoutButton.click(this.onClickedLogout.bind(this));
-        this.addButton.click(this.testDataBase.bind(this));
+        //this.addButton.click(this.testDataBase.bind(this));
+        $('select').formSelect();
+        $(document).ready(function(){
+
+        });
+        this.navigateToPlanVege = $("#vegetarianDiv"),
+        this.navigateToPlanVegan = $("#veganDiv"),
+        this.navigateToPlanDGluten = $("#glutenFreeDiv"),
+        this.navigateToPlanVege.click(this.navigateToVegetarian.bind(this));
+        this.navigateToPlanVegan.click(this.navigateToVegan.bind(this));
+        this.navigateToPlanDGluten.click(this.navigateToGlutenFree.bind(this));
+
+
+        $( "#mySelectID" ).change(function() {
+            var x = $('#mySelectID').val();
+            if(x == "1") {
+                document.getElementById('vegetarianDiv').style.display = "block";
+                document.getElementById('glutenFreeDiv').style.display = "none";
+                document.getElementById('veganDiv').style.display = "none";
+            }
+            else if(x == "2") {
+                document.getElementById('veganDiv').style.display = "block";
+                document.getElementById('vegetarianDiv').style.display = "none";
+                document.getElementById('glutenFreeDiv').style.display = "none";
+            }
+            else if(x == "3") {
+                document.getElementById('glutenFreeDiv').style.display = "block";
+                document.getElementById('vegetarianDiv').style.display = "none";
+                document.getElementById('veganDiv').style.display = "none";
+            }
+            else{
+                document.getElementById('glutenFreeDiv').style.display = "none";
+                document.getElementById('vegetarianDiv').style.display = "none";
+                document.getElementById('veganDiv').style.display = "none";
+            }
+
+
+        });
 
         firebase.auth().onAuthStateChanged(function (user) {
             if(!user){
@@ -39,17 +76,26 @@ var profileFragment = {
         });
     },
 
-    testDataBase : function (){
-        var userId = firebase.auth().currentUser.uid;
-        firebase.database().ref('test/' + userId).set({
-            username: "xxxxx",
-            email: "sewerun777@gmail.com",
-            profile_picture : "avatar.jpg"
-        });
-    },
-
     navigateToLogin : function () {
         navigation.navigateToPath(  "/view/login/login.html" );
+    },
+    navigateToVegetarian : function () {
+
+        localStorage.removeItem("diet_offer_ID");
+        localStorage.setItem("diet_offer_ID", "5");
+        navigation.navigateToPath("/view/plan_details/plan_details.html");
+    },
+    navigateToVegan : function () {
+
+        localStorage.removeItem("diet_offer_ID");
+        localStorage.setItem("diet_offer_ID", "4");
+        navigation.navigateToPath("/view/plan_details/plan_details.html");
+    },
+    navigateToGlutenFree : function () {
+
+        localStorage.removeItem("diet_offer_ID");
+        localStorage.setItem("diet_offer_ID", "1");
+        navigation.navigateToPath("/view/plan_details/plan_details.html");
     }
 
 };
