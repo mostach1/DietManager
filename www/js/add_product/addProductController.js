86bedc57178f800ea8cdf1cc68fb27ccc07cbb83
Jakub Talarczyk <jakub.talarczyk2@gmail.com>
2018-05-16 21:30:03 +0200

    search icon

--- www/js/add_product/addProductController.js
+++ www/js/add_product/addProductController.js
@@ -9,9 +9,11 @@ var controller = {
         if (navigator.userAgent.indexOf('Android') > -1)
         {
             document.getElementById('obrazek').src = "file:///android_asset/www/img/jajko.png";
+            document.getElementById('search_icon').src = "file:///android_asset/www/img/search.png";
         }
         else {
             document.getElementById('obrazek').src = "../../www/img/jajko.png";
+            document.getElementById('search_icon').src = "../../www/img/search.png";
         }
     },
 
